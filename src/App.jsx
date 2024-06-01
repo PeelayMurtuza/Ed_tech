5faba98a4092aa01b23165ef1daa3d383c719605
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-06-01 18:44:59 +0530

    Starting With Edtech

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
   return (
     <>
     <div className="">
-      <div className="bg-red-600">murtuza</div>
+      <div className="bg-red-600"></div>
     </div>
     </>
   )
