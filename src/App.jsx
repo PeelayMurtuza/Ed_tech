b66cb1ba30393481658b3a569640a0ea72797b5f
PeelayMurtuza <murtuzapeely@gmail.com>
2024-06-01 18:34:32 +0530

    Starting with Edtech

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="">
+      <div className="bg-red-600">murtuza</div>
+    </div>
+    </>
+  )
+}
+
+export default App
